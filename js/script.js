caea909347e2a2c1e544448eef1252a487681195
ulises matysiak <ulisesmatysiak2gmail.com>
2022-06-23 17:36:05 -0300

    agregando primera entrega

--- js/script.js
+++ js/script.js
@@ -48,3 +48,53 @@ let notaFaltante = notaFinal - notaEsperada;
 alert("Tu nota final es de " + notaFinal );
 alert(`Te faltaron ${notaFaltante}.`);
 }
+
+function datosDelProspecto (nombre, edad, localidad, calle) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.localidad = localidad;
+    this.calle = calle;
+}
+const prospecto1 = new datosDelProspecto (nombreProspecto, edadProspecto, localidadProspecto, calleProspecto);
+
+console.log(prospecto1);
+
+function productos (producto) {
+    this.producto = producto.nombre
+}
+
+const producto1 = new productos ({nombre : "hamburguesa"});
+const producto2 = new productos ({nombre : "papas fritas"});
+const producto3 = new productos ({nombre : "bastones"});
+const producto4 = new productos ({nombre : "Aros de cebolla"});
+const producto5 = new productos ({nombre : "gaseosa"});
+const producto6 = new productos ({nombre : "chocotorta"});
+const producto7 = new productos ({nombre : "helado"});
+
+
+console.log(producto1);
+console.log(producto2);
+console.log(producto3);
+console.log(producto4);
+console.log(producto5);
+console.log(producto6);
+console.log(producto7);
+
+
+const productosSeleccionados = [producto1, producto2, producto3, producto4, producto5];
+console.log(productosSeleccionados);
+
+
+const productosGuardadosEnFavoritos = [producto6, producto7];
+console.log(productosGuardadosEnFavoritos);
+
+const totalProductos = productosGuardadosEnFavoritos.concat(productosSeleccionados);
+console.log(totalProductos);
+
+for (const misProductos of totalProductos) {
+    console.log(`Tus productos son ${misProductos}`);
+
+}
+
+totalProductos.splice (3, 2);
+console.log(totalProductos);
