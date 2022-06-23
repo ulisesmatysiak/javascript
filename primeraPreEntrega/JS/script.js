363e3ab479f6391eb6e6ee456aa6c48bea764d78
ulises matysiak <ulisesmatysiak2gmail.com>
2022-06-23 14:20:16 -0300

    primera pre entrega

--- primeraPreEntrega/JS/script.js
+++ primeraPreEntrega/JS/script.js
@@ -1,90 +1,117 @@
-class Aceite {
-    constructor(aceite, precio) {
-        this.id = aceite.id
-        this.marca = aceite.marca
-        this.descripcion = aceite.descripcion
-        this.tipo = aceite.tipo
-        this.precio = precio
+class Alfajor {
+    constructor(alfajor, cantidad) {
+        this.id = alfajor.id;
+        this.marca = alfajor.marca;
+        this.precio = alfajor.precio;
+        this.cantidad = cantidad;
+        this.precioTotal = alfajor.precio;
     }
 
-    sacarPrecio() {
-        this.precio = precio * 1.25
+    agregarUnidad() {
+        this.cantidad++;
+    }
+
+    quitarUnidad() {
+        this.cantidad--;
+    }
+
+    actualizarPrecioTotal() {
+        this.precioTotal = this.precio * this.cantidad;
     }
 }
 
-let Aceites = [
+
+const  FiltroAceite = [
     {
         id: 0,
         marca: "Castrol",
-        descripcion: "10w-40",
+        descipcion: "10w-40",
         tipo: "Semisintetico",
         precio: 1500,
     },
     {
         id: 1,
         marca: "Mobil",
-        descripcion: "10w-50",
+        descipcion: "10w-50",
         tipo: "Sintetico",
         precio: 1000,
     },
     {
         id: 2,
         marca: "Total",
-        descripcion: "5w-40",
+        descipcion: "5w-40",
         tipo: "Semisintetico",
         precio: 1800,
     },
     {
         id: 3,
         marca: "YPF",
-        descripcion: "10w-60",
+        descipcion: "10w-60",
         tipo: "Sintetico",
         precio: 800,
     },
 ];
 
-
-Aceites.push({ id: 4,  marca: "Shell", descripcion: "10w-50", tipo: "Semisintetico", precio: 1500 })
-
-console.log(Aceites);
-
-let filtroCostos = {
-    aceite: 1200,
-    aire: 800,
-    combustible: 2000,
-    habitáculo: 800,
+function imprimirDatosTomadosPorPrompt () {
+    let carrito = document.querySelector("#carrito");
+    let dato = prompt("Cual es su nombre?");
+    let p = document.createElement("p");
+    p.textContent = `Seleccionaste ${dato}`;
+    carrito.appendChild("p");
 }
 
-function cotizador() {
-    let nombre = prompt ("Bienvenido, ingrese su nombre y apellido")
-    let filtro = prompt("Bienvenido: " + nombre + " " + "\n Ingrese la opción deseada \n"  + "¿Qué filtro necesita? \n - 1 Filtro de aceite \n - 2 Filtro de aire \n - 3 Filtro de combustible \n - 4 Filtro de habitáculo \n - 5 Salir")
-    if (filtro == 1) {
-        alert("El precio del filtro es " + "$" + (filtroCostos.aceite*1.25))
-    } else if (filtro == 2) {
-        alert("El precio del filtro es " + "$" + (filtroCostos.aire*1.25))
-    } else if (filtro == 3) {
-        alert("El precio del filtro es " + "$" + (filtroCostos.combustible*1.25))
-    } else if (filtro == 4) {
-        alert("El precio del filtro es " + "$" + (filtroCostos.habitáculo*1.25))
-    } else if (filtro == 5) {
-        return
-    } else {
-        alert("Número incorrecto. Por favor ingrese una de las opciones")
-    }
-}
-
-cotizador();
 
 
-for (let i = 0; i < Aceites.length; i++) {
-    console.log(Aceites[i]);
-}
+// function cotizador() {
+//     let nombre = prompt ("Bienvenido, ingrese su nombre y apellido")
+//     let filtro = prompt("Bienvenido: " + nombre + " " + "\n Ingrese la opción deseada \n"  + "¿Qué filtro necesita? \n - 1 Filtro de aceite \n - 2 Filtro de aire \n - 3 Filtro de combustible \n - 4 Filtro de habitáculo \n - 5 Salir")
+//     if (filtro == 1) {
+//         alert("El precio del filtro es " + "$" + (filtroCostos.aceite*1.25))
+//     } else if (filtro == 2) {
+//         alert("El precio del filtro es " + "$" + (filtroCostos.aire*1.25))
+//     } else if (filtro == 3) {
+//         alert("El precio del filtro es " + "$" + (filtroCostos.combustible*1.25))
+//     } else if (filtro == 4) {
+//         alert("El precio del filtro es " + "$" + (filtroCostos.habitáculo*1.25))
+//     } else if (filtro == 5) {
+//         return
+//     } else {
+//         alert("Número incorrecto. Por favor ingrese una de las opciones")
+//     }
+// }
 
-let PrecioNuevoEjemplo1 = []
+// cotizador();
 
-PrecioNuevoEjemplo1 = Aceites.map((aceite) => {
-    aceite.precio = aceite.precio * 1.25
-    return aceite
-})
 
-console.log(PrecioNuevoEjemplo1);
+// function imprimirElementosEnHTML(array) {
+//     let contenedor = document.getElementById("contenedor");
+//     for(const elemento of array) {
+//         let card = document.createElement("div");
+//         let nombre = document.createElement("h2");
+//         nombre.textContent = elemento.marca;
+//         card.appendChild(nombre);
+//         let precio = document.createElement("p");
+//         precio.textContent = `Precio ${elemento.precio}`;
+//         card.appendChild(precio);
+//         contenedor.appendChild(elemento);
+//     }
+// }
+
+// //imprimirElementosEnHTML(array);
+
+// function imprimirElementosEnHTML2(array) {
+//     let contenedor = document.getElementById("contenedor");
+//     for (const elemento of array) {
+//         contenedor.innerHTML = `
+// 		<div>
+// 			<h2>${elemento.nombre}</h2>
+// 			<img src="${elemento.img}">
+// 			<p>Precio: ${elemento.precio}</p>
+// 		</div>`;
+//     }
+// }
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+St
